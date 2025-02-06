cf2cf51883e552618bb3cad24de282590f7a604a
Igor Santos <igr.exe@gmail.com>
2025-02-06 12:23:16 -0300

    feat: add entpoint /create-router to save traefik config in etcd

--- tests_traefik/test_api_traefik.py
+++ tests_traefik/test_api_traefik.py
@@ -12,6 +12,12 @@ def client():
     return TestClient(app)
 
 
+@pytest.fixture
+def mock_etcd_client():
+    with unittest.mock.patch("etcd3.Client") as etcd_client:
+        yield etcd_client
+
+
 def test_status_code_return_healthcheck_200(client):
     """Testa código de status 200 para o endpoint /healthcheck"""
     response = client.get("/healthcheck")
@@ -36,3 +42,28 @@ def test_traefik_router_create(client):
     
     assert response.status_code == 201
     assert response.json() == {"status": "ok"}
+
+
+def test_etcdclient_called_when_create_router(client, mock_etcd_client):
+    """Testar chamada no client etcd para o endpoint /create-router"""
+    mock_etcd = unittest.mock.MagicMock()
+    mock_etcd_client.return_value = mock_etcd
+
+    response = client.post("/create-router", json={"service": "public", "domain_name": "test.custom.devel"})
+    
+    mock_etcd.put.assert_called()
+    assert mock_etcd.put.call_count == 5
+
+
+def test_etcdclient_called_format_when_create_router(client, mock_etcd_client):
+    """Testar chamada no client etcd para o endpoint /create-router"""
+    mock_etcd = unittest.mock.MagicMock()
+    mock_etcd_client.return_value = mock_etcd
+
+    response = client.post("/create-router", json={"service": "public", "domain_name": "test.custom.devel"})
+
+    mock_etcd.put.assert_any_call("traefik/http/routers/test-custom-devel/entrypoints", "websecure")
+    mock_etcd.put.assert_any_call("traefik/http/routers/test-custom-devel/rule", "Host(`test.custom.devel`) || Host(`www.test.custom.devel`)")
+    mock_etcd.put.assert_any_call("traefik/http/routers/test-custom-devel/service", "public@docker")
+    mock_etcd.put.assert_any_call("traefik/http/routers/test-custom-devel/tls", "true")
+    mock_etcd.put.assert_any_call("traefik/http/routers/test-custom-devel/tls/certresolver", "myresolver")
--- traefik_api/api.py
+++ traefik_api/api.py
@@ -1,3 +1,5 @@
+import etcd3
+
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, status
@@ -26,4 +28,25 @@ async def healthcheck():
 
 @app.post("/create-router", status_code=status.HTTP_201_CREATED)
 async def create_router(router: Router):
+    client = etcd3.Client("127.0.0.1", 2379)
+
+    router_key_name = router.domain_name.replace(".", "-")
+    router_config_values = {
+        "/entrypoints": "websecure",
+        "/rule": f"Host(`{router.domain_name}`) || Host(`www.{router.domain_name}`)",
+        "/service": f"{router.service}@docker",
+        "/tls": "true",
+        "/tls/certresolver": "myresolver",
+    }
+    
+    for config_key_name, value in router_config_values.items():
+        client.put(f"traefik/http/routers/{router_key_name}{config_key_name}", value)
+    
     return {"status": "ok"}
+
+
+# etcdctl put traefik/http/routers/foraderrite-org/entrypoints "websecure"
+# etcdctl put traefik/http/routers/foraderrite-org/rule "HostRegexp(\`((www\.)?([a-z0-9-]+\.)?foraderrite\.(org))$\`)"
+# etcdctl put traefik/http/routers/foraderrite-org/service "public@docker"
+# etcdctl put traefik/http/routers/foraderrite-org/tls "true"
+# etcdctl put traefik/http/routers/foraderrite-org/tls/certresolver "myresolver"
