4a4cf72c102a4c2141e179eaa181605ec2458eb4
Igor Santos <igr.exe@gmail.com>
2025-02-14 08:07:40 -0300

    feat: remove traefik api

--- traefik_api/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Router(BaseModel):
-    # Serviço Docker usado para configurar o destino do Roteador
-    service: str
-    # Endereço utilizado para configurar o Roteador
-    domain_name: str
